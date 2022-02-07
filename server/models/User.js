51b4cbd61fb048290a632e4973e5df8bec070d1f
yanghuixi1 <yanghui.xi1@gmail.com>
2022-02-07 18:21:58 -0500

    Add user model

---
+++ server/models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const validateEmail = function (email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
+// Schema to create Post model
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    validate: [validateEmail, "Please input a valid email address."],
+  },
+  hashed_password: {
+    type: String,
+    require: true,
+  },
+});
+
+userSchema.virtual("password").set(function (password) {
+  this.hashed_password = bcrypt.hashSync(password, 12);
+});
+
+userSchema.methods.isCorrectPassword = function (given_password) {
+  return bcrypt.compareSync(given_password, this.hashed_password);
+};
+
+// Initialize our User model
+const User = model("user", userSchema);
+
+module.exports = User;
