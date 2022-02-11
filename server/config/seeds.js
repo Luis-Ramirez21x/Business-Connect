dc4eaeef703ecd964f686ebcb12da38dfb1c471d
yanghuixi1 <yanghui.xi1@gmail.com>
2022-02-10 23:03:57 -0500

    Add seed data for user, review, business

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,22 +1,47 @@
-const db = require('./connection');
-const { User, Business, Tag} = require('../models');
+const db = require("./connection");
+const { User, Business, Tag, Review } = require("../models");
 
-db.once('open', async ()=>{
+db.once("open", async () => {
+  await Tag.deleteMany({});
+  await User.deleteMany({});
+  await Business.deleteMany({});
 
-    await Tag.deleteMany();
+  const tags = await Tag.insertMany([
+    { name: "Food" },
+    { name: "Mechanic" },
+    { name: "landscaping" },
+  ]);
 
-    const tags = await Tag.insertMany([
-        { name: 'Food'},
-        { name: 'Mechanic'},
-        { name: 'landscaping'}
-    ]);
+  const foodTag = await Tag.findOne({ name: "Food" });
 
-    console.log('tags seeded');
+  const user = await User.create({
+    username: "TestUser",
+    email: "test123@gmail.com",
+    password: "easypw",
+  });
 
-    process.exit();
+  const review = await Review.create({
+    title: "Great Experience",
+    description: "I ate here and the food was great",
+    createdByUser: user._id,
+  });
 
+  const business = await Business.create({
+    name: "Joe's Pizza",
+    address: "123 Easy Street",
+    description: "Good Pizza",
+    price: 10,
+    tags: [foodTag],
+    reviews: [review],
+  });
 
+  await User.findOneAndUpdate(
+    { _id: user._id },
+    { $addToSet: { myBusiness: business } },
+    { new: true }
+  );
 
+  console.log("data seeded");
 
-
-})
+  process.exit();
+});
