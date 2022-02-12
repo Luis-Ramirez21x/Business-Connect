a3702be81aae8789324bbf38424ca65759911334
GLBryan <garrettleebryan@gmail.com>
2022-02-12 11:12:40 -0500

    ready to be connected to mutation

--- client/src/components/Review/index.js
+++ client/src/components/Review/index.js
@@ -2,22 +2,73 @@ import React, { useState } from "react";
 import { Container, Col ,Card, CardImg, Button, Form } from "react-bootstrap";
 
 const Review = ({toggleReview}) => {
+  const [reviewFormData, setReviewFormData] = useState({ title: '', description: '' });
+  const [validated] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setReviewFormData({ ...reviewFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    try {
+    //   const { data } = await loginUser({
+    //     variables: { ...userFormData }
+    //   })
+    toggleReview(false)
+     
+    } catch (err) {
+      console.error(err);
+    }
+
+    setReviewFormData({
+      title: '',
+      description: '',
+    });
+  };
+
   return (
     <>
-      <li>Your Review</li>
-      <Form>
-        <Form.Row>
-          <Col>
-            <Form.Control
-              name="" 
-            />  
-          </Col>
-        </Form.Row>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Form.Group>
+        <Form.Label>Your Review</Form.Label>  
+          <Form.Control
+              name='title'
+              value={reviewFormData.title}
+              onChange={handleInputChange}
+              type='text'
+              size='md'
+              placeholder='Review Title'
+              required 
+          /> 
+          <Form.Control
+              name='description'
+              value={reviewFormData.description}
+              onChange={handleInputChange}
+              type='text'
+              size='md'
+              placeholder='Review Description'
+              required 
+          /> 
+        </Form.Group>  
+        <Button
+          disabled={!(reviewFormData.title && reviewFormData.description)}
+          type='submit'
+          variant='success'>
+          Post
+        </Button>
       </Form>
-      <Button onClick={() => toggleReview(false)}>close</Button>
     </>  
   )
-  
 };
 
 export default Review;
