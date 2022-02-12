be624b9d3286d0353cdf95239386b3d2cd78fd03
GLBryan <garrettleebryan@gmail.com>
2022-02-12 10:46:20 -0500

    start of form

--- client/src/components/Review/index.js
+++ client/src/components/Review/index.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
-import { Container, Card, CardImg, Button } from "react-bootstrap";
+import { Container, Col ,Card, CardImg, Button, Form } from "react-bootstrap";
 
 const Review = ({toggleReview}) => {
   return (
     <>
-      <li>review componenet</li>
+      <li>Your Review</li>
+      <Form>
+        <Form.Row>
+          <Col>
+            <Form.Control
+              name="" 
+            />  
+          </Col>
+        </Form.Row>
+      </Form>
       <Button onClick={() => toggleReview(false)}>close</Button>
     </>  
   )
