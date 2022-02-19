b6e40f73845aca758f8d498cdd7172545ae770e9
GLBryan <garrettleebryan@gmail.com>
2022-02-19 15:18:34 -0500

    home cleanup

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,13 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   Container,
   Col,
   Form,
   Button,
-  Dropdown,
   DropdownButton,
 } from "react-bootstrap";
 import DropdownItem from "react-bootstrap/esm/DropdownItem";
+import { BusinessList } from "../../components/BusinessList";
+import { useQuery, useLazyQuery } from "@apollo/client";
+import {
+  ALL_TAGS,
+  BUSINESSES_BY_TAG,
+  BUSINESS_SEARCH,
+} from "../../utils/queries";
 import "./index.css";
 
 // IMAGE IMPORTS
@@ -17,16 +23,6 @@ import icon1 from "../../images/twitter.png";
 import icon2 from "../../images/instagram.png";
 import icon3 from "../../images/facebook.png";
 import icon4 from "../../images/linkedin.png";
-
-import {
-  ALL_TAGS,
-  BUSINESSES_BY_TAG,
-  BUSINESS_SEARCH,
-} from "../../utils/queries";
-import { BusinessList } from "../../components/BusinessList";
-
-import { useQuery, useLazyQuery } from "@apollo/client";
-
 
 const Home = () => {
   const [searchInput, setSearchInput] = useState("");
@@ -97,16 +93,13 @@ const Home = () => {
               />
             </Col>
             <Col xs={12} md={1}>
-
               <Button className='search-btn-main' type='submit' variant='primary' size='lg'>Search</Button>
-
             </Col>
           </Form.Row>
         </Form>
       </Container>
 
       <Container className="cat-search">
-
         <h2 className="cat-header">You May Also Choose A Category Here </h2>
           <DropdownButton
             className="drop-down-btn"
@@ -119,16 +112,15 @@ const Home = () => {
             {tag_loading ? (
               <DropdownItem>loading...</DropdownItem>
             ) : (
-              tag_data.tags.map((tag) => {
-                
-              return (
-                <DropdownItem eventKey={tag.name} key={tag.name} value>
-                  {tag.name}
-                </DropdownItem>
-              );
-            })
-          )}
-        </DropdownButton>
+              tag_data.tags.map((tag) => {    
+                return (
+                  <DropdownItem eventKey={tag.name} key={tag.name} value>
+                    {tag.name}
+                  </DropdownItem>
+                );
+              })
+            )}
+          </DropdownButton>
         <Button className='cat-search-btn' onClick={callLoadBusiness}>Search</Button>
       </Container>
 
@@ -150,39 +142,32 @@ const Home = () => {
       <Container className="bot-banner-container">
         <img className="bot-banner-img" src={image}></img>
         <div className="bot-info-text">
-          
           <h1>Get Connected Today!</h1>
-            <p>Users from all over the US use The Business Connect Network to match with the highest rated services in their area. Signup now to get connected with amazing businesses today.</p>
-
+          <p>Users from all over the US use The Business Connect Network to match with the highest rated services in their area. 
+             Signup now to get connected with amazing businesses today.</p>
         </div>
       </Container>
 
       <div className="container-fluid footer-content">
         <h3 className="footer-header">Resource Links</h3>
-          <ul className="footer-resource-section">
+        <ul className="footer-resource-section">
           <a className="active-resource-link" href="#" alt="link1"><li className="resource-link"> About Us</li></a>
           <a className="active-resource-link" href="#" alt="link2"><li className="resource-link"> Meet our Team</li></a>
           <a className="active-resource-link" href="#" alt="link3"><li className="resource-link"> Contact Us</li></a>
           <a className="active-resource-link" href="#" alt="link4"><li className="resource-link"> FAQs</li></a>
-          </ul>
+        </ul>
 
-          <div className="footer-icon-main">
+        <div className="footer-icon-main">
           <a href="#" alt="icon-link1"><img className="footer-icon" src={icon1}></img></a>
           <a href="#" alt="icon-link2"><img className="footer-icon" src={icon2}></img></a>
           <a href="#" alt="icon-link3"><img className="footer-icon" src={icon3}></img></a>
           <a href="#" alt="icon-link4"><img className="footer-icon" src={icon4}></img></a>
-          </div>
-
-          <div>
-            <img className="footer-logo" src={footerLogo}></img>
-          </div>
-      </div>
-
-      {/* <Container className="container-fluid">
-        <div className="footer-content">
-        <h1>Footer Content</h1>
         </div>
-      </Container> */}
+
+        <div>
+          <img className="footer-logo" src={footerLogo}></img>
+        </div>
+      </div>
     </>
   );
 };
