fd1998b227f8a32d0d87cb3fe8ea8b4e3936fc05
Luis Ramirez <luis.ramirez21x@outlook.com>
2022-02-23 13:59:16 -0500

    creating started files for a User Profile Page, adding basic imports

---
+++ client/src/pages/MyProfile/index.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { Container, Card, CardImg, Button } from "react-bootstrap";
+import { useParams, Link } from "react-router-dom";
+import { useMutation, useQuery } from "@apollo/client";
+import Auth from "../../utils/auth";
+
+import "./index.css"
+
+const UserProfile = () =>{
+
+    return(
+        
+    )
+}
