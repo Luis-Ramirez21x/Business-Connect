0d7e522d403bd6436f6d61c182253212f1fc8fe5
Luis Ramirez <luis.ramirez21x@outlook.com>
2022-02-23 20:05:11 -0500

    my profile page renders profile info, needs styling and more features

--- client/src/pages/MyProfile/index.js
+++ client/src/pages/MyProfile/index.js
@@ -12,11 +12,20 @@ const MyProfile = () =>{
     console.log(data);
 
     return(
+    <>
+        {loading?(
+            <h2>loading...</h2>
+        ):(
         <Card>
             <CardImg className="card-img" width="100%" src={"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"} alt="Profile Picture"/>
-            <Card.Text>Hello</Card.Text>
+            <Card.Text>{data.user.username}</Card.Text>
+            <Card.Text>{data.user.email}</Card.Text>
+            <Card.Text>{data.user._id}</Card.Text>
         </Card>
-    )
+        )
+        }
+    </>
+)
 };
 
 export default MyProfile;
